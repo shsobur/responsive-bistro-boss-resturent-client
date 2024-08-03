51dfa3c8c2fb0e10f444fb743d5bf4290e3a0125
shsobur <shsoburhossen951@gmail.com>
2024-08-03 13:31:37 +0600

    Home page image slider design and responsive completed

--- src/Pages/HomePageLayout/OrderImageSlider/OrderImageSlider.jsx
+++ src/Pages/HomePageLayout/OrderImageSlider/OrderImageSlider.jsx
@@ -1,5 +1,16 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
 import SectionTitle from "../../../componentes/SectionTitle/SectionTitle";
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 import "../OrderImageSlider/OrderImageSlider.css";
+
+import slider1 from "../../../assets/home/slide1.jpg"
+import slider2 from "../../../assets/home/slide2.jpg"
+import slider3 from "../../../assets/home/slide3.jpg"
+import slider4 from "../../../assets/home/slide4.jpg"
+import slider5 from "../../../assets/home/slide5.jpg"
 
 const OrderImageSlider = () => {
   return (
@@ -10,8 +21,34 @@ const OrderImageSlider = () => {
       ></SectionTitle>
 
       <div className="main_image_slider_container">
-        <h2>Hello Image Slider</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum nobis rerum ducimus possimus commodi pariatur veritatis provident. Soluta animi repellendus modi enim repudiandae provident laudantium, temporibus dicta quod distinctio nemo.</p>
+        <Swiper
+          loop={true}
+          slidesPerView={3}
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={slider1} alt="salads"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider2} alt="pizzas"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider3} alt="soup"/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slider4} alt="desserts"/>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   );
--- src/componentes/SectionTitle/SectionTitle.jsx
+++ src/componentes/SectionTitle/SectionTitle.jsx
@@ -1,11 +1,28 @@
 import "../SectionTitle/SectionTitle.css";
 
-const SectionTitle = ({heading, subHeading}) => {
+const SectionTitle = ({ heading, subHeading }) => {
   return (
     <div className="main_section_title_contaienr">
-      <p>{subHeading}</p>
-      <div className="headeing_contaienr">
-        <h2>{heading}</h2>
+      <p
+        data-aos="fade-up"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000"
+      >
+        {subHeading}
+      </p>
+      <div
+        className="headeing_contaienr"
+        data-aos="flip-up"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000"
+      >
+        <h2
+          data-aos="fade-up"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="2000"
+        >
+          {heading}
+        </h2>
       </div>
     </div>
   );
